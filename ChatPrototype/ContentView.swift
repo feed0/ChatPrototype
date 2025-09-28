c69d6f12c18db724780eb90d75f4e362964ed3e4
feed0 <me@felipecampelo.com.br>
2025-09-28 16:45:13 -0300

    1* Wrap-up: Explore Xcode

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -13,7 +13,12 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Knock, knock!")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+            Text("Who's there?")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
         }
         .padding()
     }
